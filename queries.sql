78694d2ceebc15ac96acebdc624a8e9bf82ed597
Ellen Jardon <e@ellens-mbp.lan>
2023-11-01 10:09:30 -0500

    task one complete, check sql queries are as LC intended

--- queries.sql
+++ queries.sql
@@ -1,5 +1,10 @@
 --Part 1
 
+--column: techjob_id INTEGER PRIMARY KEY AUTO_INCREMENT
+--column: name VARCHAR(40)
+--column: employer VARCHAR (50)
+--column: skill VARCHAR (200)
+
 --Part 2
 
 --Part 3
